d110429fc5a366550ce6b03dd540e9ac78ba56a0
Noy! <78952684+noyexcl@users.noreply.github.com>
2026-02-28 12:24:51 +0900

    build: fix defineConfig arugment to return object correctly

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'github' ? '/remote-shogi-ban/' : '/',
-  plugins: [react(), tailwindcss()],
-}))
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'github' ? '/remote-shogi-ban/' : '/',
+    plugins: [react(), tailwindcss()],
+  }
+})
