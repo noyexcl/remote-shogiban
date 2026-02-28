6d535a0a7388d881a9be908874d1de8928ec1727
Noy! <78952684+noyexcl@users.noreply.github.com>
2026-02-28 11:58:18 +0900

    ci: set vite-base according to mode variable

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/remote-shogi-ban/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github' ? '/remote-shogi-ban/' : '/',
   plugins: [react(), tailwindcss()],
-})
+}))
